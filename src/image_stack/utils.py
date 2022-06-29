64cae33b31ec0ff50f54392aeed4e9ddb1d5e0d8
gjjring <manley@zib.de>
2022-06-29 14:50:39 +0200

    added function for checking iterables

--- src/image_stack/utils.py
+++ src/image_stack/utils.py
@@ -1,7 +1,11 @@
 import numpy as np
+from collections.abc import Iterable
 
 def gaussian(params, z):
     a = params[0]
     b = params[1]
     c = params[2]
     return a*np.exp( -(z-b)**2/(2*c**2))
+
+def is_iterable(obj):
+    return isinstance(obj, Iterable) and not isinstance(obj, str)
