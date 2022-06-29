5026c79aa5a509e9c2004e6362f4440ac09d353f
gjjring <manley@zib.de>
2022-06-29 14:11:51 +0200

    exposed JCMsuite support function at package level

--- src/image_stack/__init__.py
+++ src/image_stack/__init__.py
@@ -1,6 +1,7 @@
 from image_stack.image1d import Image1D, ImageStack1D
 from image_stack.image2d import Image2D, ImageStack2D
 from image_stack.basis_functions import BasisFunctions
+
 __version__ = "0.0.1"
 __all__ = ["Image1D", "ImageStack1D", "Image2D", "ImageStack2D",
-           "BasisFunctions"]
+           "BasisFunctions", "cartesianfields_to_image2D"]
