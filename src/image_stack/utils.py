1a91104d330f6f9a4db44bb4c5e931c98e7ecd6b
gjjring <manley@zib.de>
2022-09-29 11:06:01 +0200

    added gaussian2D function

--- src/image_stack/utils.py
+++ src/image_stack/utils.py
@@ -7,5 +7,12 @@ def gaussian(params, z):
     c = params[2]
     return a*np.exp( -(z-b)**2/(2*c**2))
 
+def gaussian2D(params, x, y):
+    a = params[0]
+    b = params[1]
+    c = params[2]
+    d = params[3]
+    return a*np.exp( -1*((x-b)**2/(2*d**2) + (y-c)**2/(2*d**2)))
+
 def is_iterable(obj):
     return isinstance(obj, Iterable) and not isinstance(obj, str)
